512e69e3b231f5f95715b2848f6a60a1e3a109d6
QiZhang <qzhang1997@gmail.com>
2020-03-11 22:45:35 +0800

    Head file bug

--- minix/servers/vm/mmap.c
+++ minix/servers/vm/mmap.c
@@ -2,16 +2,19 @@
 #define _SYSTEM 1
 
 #include <minix/callnr.h>
-#include <minix/com.h>
+// #include <minix/com.h>
+#include "include/minix/com.h"
 #include <minix/config.h>
 #include <minix/const.h>
 #include <minix/ds.h>
 #include <minix/endpoint.h>
 #include <minix/minlib.h>
 #include <minix/type.h>
-#include <minix/ipc.h>
+// #include <minix/ipc.h>
+#include "include/minix/ipc.h"
 #include <minix/sysutil.h>
-#include <minix/syslib.h>
+// #include <minix/syslib.h>
+#include "include/minix/syslib.h"
 #include <minix/safecopies.h>
 #include <minix/bitmap.h>
 #include <minix/debug.h>
