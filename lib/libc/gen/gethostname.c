006385301e216d9aee74554d7acc45826f5a6ed0
wanxinyu <345406629@qq.com>
2021-03-04 20:14:35 +0800

    delete useless function

--- lib/libc/gen/gethostname.c
+++ lib/libc/gen/gethostname.c
@@ -74,33 +74,3 @@ gethostname(char *name, size_t namelen)
 	return (0);
 }
 
-/*
-int sysctl_uts(void * oldp, size_t * oldlenp, const void * newp, size_t newlen)
-{
-	message m;
-	int r;
-
-	memset(&m, 0, sizeof(m));
-	m.m_lc_mib_sysctl.oldp = (vir_bytes)oldp;
-	m.m_lc_mib_sysctl.oldlen = (oldlenp != NULL) ? *oldlenp : 0;
-	m.m_lc_mib_sysctl.newp = (vir_bytes)newp;
-	m.m_lc_mib_sysctl.newlen = newlen;
-	
-	int mib[2];
-	mib[0] = CTL_KERN;
-	mib[1] = KERN_HOSTNAME;
-	m.m_lc_mib_sysctl.namep = (vir_bytes)mib;
-	m.m_lc_mib_sysctl.namelen = 2;
-	memcpy(m.m_lc_mib_sysctl.name, mib, sizeof(*mib) * 2);
-	
-	m.m_lc_mib_sysctl.uts_pendpt = getppid();   
-	m.m_lc_mib_sysctl.uts_cendpt = 0;	
-
-	r = _syscall(MIB_PROC_NR, MIB_SYSCTL, &m);
-	
-	if (oldlenp != NULL)
-		*oldlenp = m.m_mib_lc_sysctl.oldlen;
-	
-	return r;
-}
-*/
